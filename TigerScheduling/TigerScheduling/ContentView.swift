74d8fed69afde8cb9b78e8a47524e7d6b1a17714
Joseph Laudati <laudatijoseph@gmail.com>
2024-11-06 22:32:04 -0500

    Initial commit

---
+++ TigerScheduling/TigerScheduling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TigerScheduling
+//
+//  Created by Joseph Laudati on 11/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TigerScheduling/TigerScheduling/TigerSchedulingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TigerSchedulingApp.swift
+//  TigerScheduling
+//
+//  Created by Joseph Laudati on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TigerSchedulingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
